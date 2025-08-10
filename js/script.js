f355e08fe707980d6f0708f8d90be27cead84751
sunny <balvindersingh@live.co.uk>
2025-08-10 18:25:44 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,1 @@
+const btn=document.getElementById("menu-btn"),menu=document.getElementById("menu");function navToggle(){btn.classList.toggle("open"),menu.classList.toggle("flex"),menu.classList.toggle("hidden")}btn.addEventListener("click",navToggle);
